6721bba0ef84965eebe6e4c128751546ace5ed1a
kisukalat <kisukalat@incel.email>
2024-11-08 00:07:45 +0200

    Update index.ts

--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,12 +1,28 @@
-import { logger } from "@vendetta";
-import Settings from "./Settings";
+import { findByProps } from "@vendetta/metro";
+import { before } from "@vendetta/patcher";
+import { storage } from "@vendetta/plugin";
 
-export default {
-    onLoad: () => {
-        logger.log("Hello world!");
-    },
-    onUnload: () => {
-        logger.log("Goodbye, world.");
-    },
-    settings: Settings,
-}
+const uploadModule = findByProps("uploadLocalFiles");
+
+export const onUnload = before("uploadLocalFiles", uploadModule, (args) => { 
+    const { items } = args[0];
+    if (!items) return;
+
+    const rawLength = parseInt(storage.nameLength);
+    const length = isNaN(rawLength) ? 8 : rawLength;
+
+    for (const i of items) {
+        // https://github.com/Vendicated/Vencord/blob/7c514e4b1dae25f48b20bc6d5f3025c22e231450/src/plugins/anonymiseFileNames.ts#L70-L71
+        const extIdx = i.filename.lastIndexOf(".");
+        const ext = extIdx !== -1 ? i.filename.slice(extIdx) : "";
+
+        const name = `${Math.floor(Date.now() / 1000)}${Math.floor(window.performance.now())}`;
+
+        // why are there two. why???
+        // and yes, i checked, setting both is required...
+        i.filename = name + ext;
+        if (i.item) i.item.filename = name + ext;
+    }
+});
+
+export { default as settings } from "./Settings";
