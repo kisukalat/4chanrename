5d113bd15f15d2cf59bcd76eb47b7e46b7a7c86b
kisukalat <kisukalat@incel.email>
2025-06-20 18:56:35 +0300

    Update index.ts

--- plugins/4chan/src/index.ts
+++ plugins/4chan/src/index.ts
@@ -2,9 +2,9 @@ import { findByProps } from "@vendetta/metro";
 import { before } from "@vendetta/patcher";
 import { storage } from "@vendetta/plugin";
 
-const uploadModule = findByProps("uploadItems");
+const uploadModule = findByProps("uploadFiles");
 
-export const onUnload = before("uploadItems", uploadModule, (args) => { 
+export const onUnload = before("uploadFiles", uploadModule, (args) => { 
     const { items } = args[0];
     if (!items) return;
 
